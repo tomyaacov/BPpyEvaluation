0bf76d60a102c2925e41f8555211f5f68d359433
tomyaacov <tomyaacov1210@gmail.com>
2024-01-02 11:22:01 +0200

    updating experiment

--- drl/scripts/run_main_single_dfs.sh
+++ drl/scripts/run_main_single_dfs.sh
@@ -2,8 +2,8 @@
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
 #SBATCH --partition main ### specify partition name where to run a job
 #SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
-#SBATCH --job-name run_pancake_dfs ### name of the job. replace my_job with your desired job name
-#SBATCH --output run_pancake_dfs.out ### output log for running job - %J is the job number variable
+#SBATCH --job-name run_pancake_dfs2 ### name of the job. replace my_job with your desired job name
+#SBATCH --output run_pancake_dfs2.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=32G ### total amount of RAM // 500
@@ -18,22 +18,22 @@ module load anaconda ### load anaconda module
 source activate BPpyLiveness ### activating Conda environment. Environment must be configured before running the job
 cd ~/repos/BPpyEvaluation/drl || exit
 
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 200 --m 25
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 200 --m 50
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 200 --m 75
 ~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 200 --m 100
 
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 300 --m 25
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 300 --m 50
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 300 --m 75
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 300 --m 100
+
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 400 --m 25
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 400 --m 50
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 400 --m 75
 ~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 400 --m 100
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 400 --m 300
 
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 600 --m 100
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 600 --m 300
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 600 --m 500
-
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 800 --m 100
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 800 --m 300
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 800 --m 500
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 800 --m 700
-
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 1000 --m 100
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 1000 --m 300
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 1000 --m 500
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 1000 --m 700
-~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 1000 --m 900
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 500 --m 25
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 500 --m 50
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 500 --m 75
+~/.conda/envs/BPpyLiveness/bin/python pancake_dfs.py --n 500 --m 100
