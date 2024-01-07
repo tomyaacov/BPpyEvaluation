425800ab5542e5815dbeddb6d9ac641a551d727f
tomyaacov <tomyaacov1210@gmail.com>
2024-01-07 08:44:22 +0200

    updating experiment

--- drl/scripts/run_single_trace_search.sh
+++ drl/scripts/run_single_trace_search.sh
@@ -2,8 +2,8 @@
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
 #SBATCH --partition main ### specify partition name where to run a job
 #SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
-#SBATCH --job-name run_single_trace_search ### name of the job. replace my_job with your desired job name
-#SBATCH --output run_single_trace_search.out ### output log for running job - %J is the job number variable
+#SBATCH --job-name run_single_trace_search2 ### name of the job. replace my_job with your desired job name
+#SBATCH --output run_single_trace_search2.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=32G ### total amount of RAM // 500
@@ -22,11 +22,11 @@ options=(
 "500 25" "500 50" "500 75" "500 100"
 )
 
-echo "option,run,time,memory" > run_single_trace_search_output.csv
+echo "option,run,time,memory" > run_single_trace_search_output2.csv
 for option in "${options[@]}"; do
   echo "$option"
-  for i in {1..10}
+  for i in {1..5}
   do
-    timeout 120m /usr/bin/time -a -o run_single_trace_search_output.csv -f "$option,$i,%E,%M" python pancake_single_trace_search.py $option
+    timeout 60m /usr/bin/time -a -o run_single_trace_search_output2.csv -f "$option,$i,%E,%M" python pancake_single_trace_search.py $option
   done
 done
