0878e03a8a464f74fc1c54f6370901b76ac5194d
tomyaacov <tomyaacov1210@gmail.com>
2024-01-11 15:33:24 +0200

    updating experiment

--- drl/scripts/run_single_trace_search.sh
+++ drl/scripts/run_single_trace_search.sh
@@ -2,8 +2,8 @@
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
 #SBATCH --partition main ### specify partition name where to run a job
 #SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
-#SBATCH --job-name run_single_trace_search2 ### name of the job. replace my_job with your desired job name
-#SBATCH --output run_single_trace_search2.out ### output log for running job - %J is the job number variable
+#SBATCH --job-name run_single_trace_search3 ### name of the job. replace my_job with your desired job name
+#SBATCH --output run_single_trace_search3.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=32G ### total amount of RAM // 500
@@ -11,7 +11,7 @@
 
 ### Start you code below ####
 module load anaconda ### load anaconda module
-source activate BPpyLiveness ### activating Conda environment. Environment must be configured before running the job
+source activate bppy-py39 ### activating Conda environment. Environment must be configured before running the job
 cd ~/repos/BPpyEvaluation/drl || exit
 
 #options = ()
