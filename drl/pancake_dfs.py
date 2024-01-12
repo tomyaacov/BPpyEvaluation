4e3e36aae08c847994284d6020778d0f1ac4c484
tomyaacov <tomyaacov1210@gmail.com>
2024-01-12 09:00:24 +0200

    updating experiment

--- drl/pancake_dfs.py
+++ drl/pancake_dfs.py
@@ -4,6 +4,7 @@ from pancake import init_bprogram, get_event_list, get_action_list
 import pickle
 from bppy.model.event_selection.simple_event_selection_strategy import SimpleEventSelectionStrategy
 import random
+import time
 
 
 parser = argparse.ArgumentParser()
@@ -15,46 +16,51 @@ M = int(args.parameters[1])
 TESTED_TRACES = int(args.parameters[2])
 
 dfs = DFSBProgram(lambda: init_bprogram(N, M), get_event_list(), max_trace_length=10 ** 10, interrupt_on_trace=False)
+time_start = time.time()
 init_s, visited = dfs.run()
-
-before = 0
-after = 0
-for x in visited:
-    if hasattr(x, "flagged"):
-        after += 1
-    else:
-        x.flagged = False
-    for k, v in x.transitions.items():
-        if not hasattr(v, "flagged"):
-            v.flagged = False
-while before != after:
-    before = after
-    for x in visited:
-        if x.flagged:
-            continue
-        if any([y.flagged for y in x.transitions.values()]):
-            x.flagged = True
-            after += 1
-
-def generate_trace(init_s, visited, good):
-    current_s = init_s
-    trace = []
-    ess = SimpleEventSelectionStrategy()
-    while True:
-        if len(ess.selectable_events([x.data for x in current_s.nodes if x.data is not None])) == 0:
-            return trace, current_s.flagged
-        if good:
-            e, current_s = random.choice([(k, v) for k, v in current_s.transitions.items() if v.flagged])
-        else:
-            e, current_s = random.choice([(k, v) for k, v in current_s.transitions.items()])
-        current_s = visited[visited.index(current_s)]
-        trace.append(e)
+time_end = time.time()
+print("Time: " + str(time_end - time_start))
+print("States: " + str(len(visited)))
 
 
-traces = []
-for i in range(TESTED_TRACES):
-    traces.append(generate_trace(init_s=init_s, visited=visited, good=i % 2 == 0))
-
-
-with open('traces.pkl', 'wb') as f:
-    pickle.dump(traces, f)
+# before = 0
+# after = 0
+# for x in visited:
+#     if hasattr(x, "flagged"):
+#         after += 1
+#     else:
+#         x.flagged = False
+#     for k, v in x.transitions.items():
+#         if not hasattr(v, "flagged"):
+#             v.flagged = False
+# while before != after:
+#     before = after
+#     for x in visited:
+#         if x.flagged:
+#             continue
+#         if any([y.flagged for y in x.transitions.values()]):
+#             x.flagged = True
+#             after += 1
+#
+# def generate_trace(init_s, visited, good):
+#     current_s = init_s
+#     trace = []
+#     ess = SimpleEventSelectionStrategy()
+#     while True:
+#         if len(ess.selectable_events([x.data for x in current_s.nodes if x.data is not None])) == 0:
+#             return trace, current_s.flagged
+#         if good:
+#             e, current_s = random.choice([(k, v) for k, v in current_s.transitions.items() if v.flagged])
+#         else:
+#             e, current_s = random.choice([(k, v) for k, v in current_s.transitions.items()])
+#         current_s = visited[visited.index(current_s)]
+#         trace.append(e)
+#
+#
+# traces = []
+# for i in range(TESTED_TRACES):
+#     traces.append(generate_trace(init_s=init_s, visited=visited, good=i % 2 == 0))
+#
+#
+# with open('traces.pkl', 'wb') as f:
+#     pickle.dump(traces, f)
