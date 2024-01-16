aab3403724967af8f2adf612a7746abe983cb46d
tomyaacov <tomyaacov1210@gmail.com>
2024-01-16 08:58:41 +0200

    updating experiment

--- drl/scripts/run_multiple_traces_qrdqn_200_25.sh
+++ drl/scripts/run_multiple_traces_qrdqn_200_25.sh
@@ -2,8 +2,8 @@
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
 #SBATCH --partition main ### specify partition name where to run a job
 #SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
-#SBATCH --job-name run_multiple_traces_qrdqn_200_25 ### name of the job. replace my_job with your desired job name
-#SBATCH --output run_multiple_traces_qrdqn_200_25.out ### output log for running job - %J is the job number variable
+#SBATCH --job-name run_multiple_traces_qrdqn_200_25_2 ### name of the job. replace my_job with your desired job name
+#SBATCH --output run_multiple_traces_qrdqn_200_25_2.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=32G ### total amount of RAM // 500
