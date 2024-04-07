c6cc5fbddae0dd86d15bc586d9269ee818a2764f
adielas <adiel.ashrov@gmail.com>
2024-04-07 18:27:19 +0300

    Support runtime and memory measurements for the bit_flipping_smt.py example as well.

--- smt_solvers/bit_flipping_experiments.py
+++ smt_solvers/bit_flipping_experiments.py
@@ -1,24 +1,36 @@
-import math
-
-from bppy import BEvent
-import itertools
-import bppy as bp
-import tracemalloc
 import datetime
 import csv
 from bit_flipping_discrete import run_bit_flipping_discrete_bp_program
+from bit_flipping_smt import run_bit_flipping_smt_bp_program
+
 def init_statistics_file():
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     filename = f"statistics_{timestamp}.csv"
     return filename
 
-def run_experiemnts(csvfile, max_n, max_m, number_of_experiments=1):
 
+def parse_arguments():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n", "--n", type=int, default=3, help="Number of rows"
+    )
+    parser.add_argument(
+        "-m", "--m", type=int, default=3, help="Number of cols"
+    )
+    parser.add_argument(
+        "-n_e", "--num_of_exp", type=int, default=3, help="The number of times to run the experiment"
+    )
+    args = parser.parse_args()
+    return args.n, args.m, args.num_of_exp
+
+def run_experiemnts(csvfile, n, m, number_of_experiments=1):
 
     # Init a dictionary to store n*m keys and the appropriate n,m value
     n_x_m_dict  = {}
-    for n in range(2, max_n + 1):
-        for m in range(2, max_m + 1):
+    for n in range(2, n + 1):
+        for m in range(2, m + 1):
             if n*m not in n_x_m_dict:
                 n_x_m_dict[n*m] = (n,m)
 
@@ -30,8 +42,8 @@ def run_experiemnts(csvfile, max_n, max_m, number_of_experiments=1):
         "(n,m)",
         "execution_time_discrete",
         "memory_usage_discrete",
-        "execution_time_solver",
-        "memory_usage_solver",
+        "execution_time_smt",
+        "memory_usage_smt",
     ]
 
     writer = csv.writer(csvfile, delimiter=",")
@@ -40,28 +52,41 @@ def run_experiemnts(csvfile, max_n, max_m, number_of_experiments=1):
 
     execution_time_discrete_dict = {}
     memory_usage_discrete_dict = {}
+    execution_time_smt_dict = {}
+    memory_usage_smt_dict = {}
 
     for exp in range(number_of_experiments):
         for key in n_x_m_dict.keys():
                 n,m = n_x_m_dict[key]
                 execution_time_discrete,memory_usage_discrete = run_bit_flipping_discrete_bp_program( n, m )
+                execution_time_smt, memory_usage_smt = run_bit_flipping_smt_bp_program(n, m)
                 if key not in execution_time_discrete_dict:
                     execution_time_discrete_dict[key] = execution_time_discrete
                     memory_usage_discrete_dict[key] = memory_usage_discrete
+                    execution_time_smt_dict[key] = execution_time_smt
+                    memory_usage_smt_dict[key] = memory_usage_smt
                 else:
                     execution_time_discrete_dict[key] += execution_time_discrete
-                    memory_usage_discrete_dict[key] = memory_usage_discrete
+                    memory_usage_discrete_dict[key] += memory_usage_discrete
+                    execution_time_smt_dict[key] += execution_time_smt
+                    memory_usage_smt_dict[key] += memory_usage_smt
 
     for key in n_x_m_dict.keys():
         execution_time_discrete_dict[key] /= number_of_experiments # average the execution time
         execution_time_discrete_dict[key] = 1000 * execution_time_discrete_dict[key] # From seconds to milliseconds
-        # print(f"{key} Avg. Execution time: ", execution_time_discrete_dict[key])
+        # print(f"{key} Avg. Execution time discrete: ", execution_time_discrete_dict[key])
 
-        memory_usage_discrete_dict[key] /= number_of_experiments  # average the execution time
-        memory_usage_discrete_dict[key] = 1000 * memory_usage_discrete_dict[key]  # From seconds to milliseconds
-        # print(f"{key} Avg. Memory usage: ", memory_usage_discrete_dict[key])
+        memory_usage_discrete_dict[key] /= number_of_experiments  # average the memory usage,units in KB
+        # print(f"{key} Avg. Memory usage discrete: ", memory_usage_discrete_dict[key])
 
-        row = [key, n_x_m_dict[key], execution_time_discrete_dict[key], memory_usage_discrete_dict[key], 0.0, 0.0]
+        execution_time_smt_dict[key] /= number_of_experiments # average the execution time
+        execution_time_smt_dict[key] = 1000 * execution_time_smt_dict[key] # From seconds to milliseconds
+        # print(f"{key} Avg. Execution time smt: ", execution_time_smt_dict[key])
+
+        memory_usage_smt_dict[key] /= number_of_experiments  # average the memory usage, units in KB
+        # print(f"{key} Avg. Memory usage: smt", memory_usage_smt_dict[key])
+
+        row = [key, n_x_m_dict[key], execution_time_discrete_dict[key], memory_usage_discrete_dict[key], execution_time_smt_dict[key], memory_usage_smt_dict[key]]
         writer.writerow(row)
         print(",".join([str(x) for x in row]))
 
@@ -69,7 +94,8 @@ if __name__ == '__main__':
     try:
         with open(init_statistics_file(), mode="w", newline="") as csvfile:
             current_datetime = datetime.datetime.now()
-            run_experiemnts(csvfile, 3, 3, 3)
+            n, m, num_of_exp = parse_arguments()
+            run_experiemnts(csvfile, n, m, num_of_exp)
     except KeyboardInterrupt:
         # this code handles keyboard interrupt
         print("Keyboard interrupt")
