a9128f90803cd1977c5620403c50f6d0d21332ba
tomyaacov <tomyaacov1210@gmail.com>
2024-05-12 20:21:53 +0300

    updating instructions

--- drl/scripts/cinderella_multiple_traces.sh
+++ drl/scripts/cinderella_multiple_traces.sh
@@ -2,4 +2,4 @@
 echo "DQN:"
 python3 cinderella_multiple_traces_drl.py 5 50 2 5 1000 1000000 DQN
 echo "QRDQN:"
-python3 cinderella_multiple_traces_drl.py 5 50 2 5 1000 1000000  QRDQN
+python3 cinderella_multiple_traces_drl.py 5 50 2 5 1000 1000000 QRDQN
